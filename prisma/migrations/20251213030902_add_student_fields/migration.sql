120d9028da1b68c57f94cd5652540d970b4e4bc9
Pedro Passos <pedro.passos081@gmail.com>
2025-12-13 00:09:34 -0300

    chore(prisma): add student guardian and birthDate fields

---
+++ prisma/migrations/20251213030902_add_student_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Student" ADD COLUMN     "birthDate" TIMESTAMP(3),
+ADD COLUMN     "guardianEmail" TEXT,
+ADD COLUMN     "guardianName" TEXT,
+ADD COLUMN     "guardianPhone" TEXT;
